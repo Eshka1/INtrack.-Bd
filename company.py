ab94513302989d49d229588e455ed584bbc63d34
farhanahmed168 <farhan.ahmed1@g.bracu.ac.bd>
2026-08-31 11:43:32 +0600

    Delete company.py

--- company.py
+++
@@ -1,9 +0,0 @@
-from beanie import Document
-class Company(Document):
-    company_id:str
-    company_name:str
-    uid:str
-    password:str
-    subscription_id:str
-    class Settings:
-        name='companies'
