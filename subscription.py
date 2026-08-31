188e485053919e507eade352923e7c6edbf8887b
farhanahmed168 <farhan.ahmed1@g.bracu.ac.bd>
2026-08-31 11:39:48 +0600

    Delete subscription.py

--- subscription.py
+++
@@ -1,9 +0,0 @@
-from beanie import Document
-class Subscription(Document):
-    subscription_id:str
-    plan_name:str
-    start_date:str
-    monthly_price:float
-    yearly_price:float
-    class Settings:
-        name='subscriptions'
