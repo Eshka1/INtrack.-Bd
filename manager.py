b6055d560156695f7c6a01877645fd97b2ab41af
farhanahmed168 <farhan.ahmed1@g.bracu.ac.bd>
2026-08-31 11:42:40 +0600

    Delete manager.py

--- manager.py
+++
@@ -1,9 +0,0 @@
-from beanie import Document
-class Manager(Document):
-    manager_id:str
-    company_id:str
-    name:str
-    email:str
-    password:str
-    class Settings:
-        name='managers'
